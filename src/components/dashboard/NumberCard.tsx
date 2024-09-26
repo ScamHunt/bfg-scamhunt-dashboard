09e6a6bcd539e3d4a7d024dd25e88eb67143e0e6
Kazymir Rabier <kazymir.rabier@gmail.com>
2024-09-26 20:12:20 +0800

    add new chart likeliness + refresh ui

--- src/components/dashboard/NumberCard.tsx
+++ src/components/dashboard/NumberCard.tsx
@@ -1,26 +1,22 @@
-import { Card, CardHeader, CardTitle, CardContent } from "../ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 interface DataCardProps {
-  title: string;
-  content: number;
-  subtitle?: string;
+  content: { title: string; data: string }[];
 }
 
-export const NumberCard: React.FC<DataCardProps> = ({
-  title,
-  content,
-  subtitle,
-}) => {
+export const NumberCard: React.FC<DataCardProps> = ({ content }) => {
   return (
     <Card>
-      <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-        <CardTitle className='text-sm font-medium'>{title}</CardTitle>
-        {/* <Icon className='text-blue-400' />; */}
-      </CardHeader>
-      <CardContent>
-        <div className='text-2xl font-bold'>{content}</div>
-        <p className='text-xs text-zinc-500 dark:text-zinc-400'>{subtitle}</p>
-      </CardContent>
+      {content.map((item, index) => (
+        <>
+          <CardHeader key={index}>
+            <CardTitle>{item.title}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className='text-xl font-bold'>{item.data.charAt(0).toUpperCase() + item.data.slice(1)}</div>
+          </CardContent>
+        </>
+      ))}
     </Card>
   );
 };
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,17 @@
 import { ShieldAlert } from "lucide-react";
+import Image from "next/image";
 
 export const Navbar = () => {
   return (
-    <nav className='border-b border-gray-200 dark:border-gray-800'>
+    <nav className='bg-green-950 text-white font-bold border-b border-gray-200 dark:border-gray-800'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex justify-between h-16'>
           <div className='flex'>
             <div className='flex-shrink-0 flex items-center'>
-              <ShieldAlert className='h-8 w-8 text-neon-green dark:text-green-400' />
-              <span className='ml-2 text-2xl font-bold'>ScamHunt</span>
+              <Image src={`/logo.jpeg`} alt='ScamHunt' width='48' height='48' />
+              <span className='ml-2 text-xl'>
+                ScamHunt Dashboard For Law Enforcement Authorities
+              </span>
             </div>
           </div>
           <div className='flex items-center'></div>
